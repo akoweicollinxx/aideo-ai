6ea40919c73ca002541715cf749676ac7d3a99db
akoweicollinxx <akoweicollins17@gmail.com>
2026-03-17 13:26:42 +0000

    feat: initial commit

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,7 +1,13 @@
+import CompanionForm from "@/components/CompanionForm";
 
 const Page = () => {
     return (
-        <div>New Companion</div>
+        <main className="min-lg:w-1/3 min-md:w-2/3 justify-center items-center">
+            <article className="w-full gap-4 flex flex-col">
+                <h1>Companion Builder</h1>
+                <CompanionForm/>
+            </article>
+        </main>
     )
 }
 export default Page
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,47 @@
 import React from 'react'
 import {Button} from "@/components/ui/button";
+import CompanionCards from "@/components/CompanionCards";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
+
 
 const Page = () => {
   return (
-    <div>
-        <h1 className="text-2xl underline">Welcome to Aideo</h1>
-        <Button>Lets get Started</Button>
-    </div>
+    <main>
+        <h1 className="text-2xl underline">popular Companions</h1>
+        <section className="home-section">
+            <CompanionCards
+                id="123"
+                name="Neura the Brainy Explorer"
+                subject="science"
+                duration={45}
+                color="#ffda6e" topic={""} bookmarked={false}            />
+
+            <CompanionCards
+                id="456"
+                name="Collins the Brainy Developer"
+                subject="javascript"
+                duration={45}
+                color="#ffda6e" topic={""} bookmarked={false}            />
+
+            <CompanionCards
+                id="789"
+                name="Asake the Brainy Musician"
+                subject="Music"
+                duration={45}
+                color="#ffda6e" topic={""} bookmarked={false}            />
+
+        </section>
+
+        <section className="home-section">
+            <CompanionsList title="Recently completer Sessions"
+            companions={recentSessions} classNames="w-2/3 max-lg:w-full"
+            />
+            <CTA/>
+        </section>
+
+    </main>
   )
 }
 
